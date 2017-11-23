8acc2ae915bb3195e3d0a41f85c9187251d164c4
Korkiat90 <dak1990@hotmail.de>
2017-11-23 18:03:40 +0100

    StartSzene Hinzugefügt + Hamsterabschuss

---
+++ GoingHamster/Assets/Scripts/HamsterScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HamsterScript : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public Rigidbody2D hook;
+    public float releaseTime = 0.15f;
+    public float MaxDragDistance = 2f;
+
+
+    private bool isPressed = false;
+
+    void Update()
+    {
+        if (isPressed)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector3.Distance(mousePos, hook.position) > MaxDragDistance)
+                rb.position = hook.position + (mousePos - hook.position).normalized * MaxDragDistance;
+
+            else
+                rb.position = mousePos;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release()
+    {
+
+        yield return new WaitForSeconds(releaseTime);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+
+        this.enabled = false;
+    }
+}
