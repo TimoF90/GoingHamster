72fe90b691bfeb0fd89c626a3de5054ea33987d7
TimoF90 <33117636+TimoF90@users.noreply.github.com>
2017-11-29 14:07:44 +0100

    PauseMenu

---
+++ GoingHamster/Assets/Scripts/CoinScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+        void OnCollisionEnter2D(Collision2D coll)
+        {
+            if (coll.gameObject.tag.Equals ("Player"))
+            {
+                ScoreScript.scoreValue += 10;
+                Destroy(gameObject);
+            }
+
+
+        
+    }
+}
